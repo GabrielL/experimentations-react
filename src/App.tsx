7faef9926950139a400670f7ede4c6b093accf35
Gabriel Laskar <gabriel@mantra.ms>
2025-01-04 11:22:38 +0100

    start using MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,35 @@
 import { useState } from "react";
 
+import {
+  Button,
+  Card,
+  CardContent,
+  Container,
+  Paper,
+  Typography,
+} from "@mui/material";
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Container component={Paper}>
+        <Typography variant="h1">Vite + React</Typography>
+        <Card>
+          <CardContent>
+            <Button
+              variant="outlined"
+              onClick={() => setCount((count) => count + 1)}
+            >
+              count is {count}
+            </Button>
+            <Typography>
+              Edit <code>src/App.tsx</code> and save to test HMR
+            </Typography>
+          </CardContent>
+        </Card>
+      </Container>
     </>
   );
 }
