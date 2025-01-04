69c040217cf81b36fddaa6f7077ad838bc09a915
Gabriel Laskar <gabriel@mantra.ms>
2025-01-04 15:48:20 +0100

    better read viewer

--- src/pages/jokes/allJokes.tsx
+++ src/pages/jokes/allJokes.tsx
@@ -1,7 +1,15 @@
-import { Box } from "@mui/material";
+import DraftsIcon from "@mui/icons-material/Drafts";
+import MarkunreadIcon from "@mui/icons-material/Markunread";
+import { Box, useTheme } from "@mui/material";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
 
 import { Joke } from "../../services/jokes.ts";
+
+const ReadIcon = ({ viewed = false }) => {
+  const theme = useTheme();
+  if (viewed) return <DraftsIcon htmlColor={theme.palette.success.main} />;
+  else return <MarkunreadIcon />;
+};
 
 type AllJokesProps = {
   jokes: Joke[];
@@ -18,6 +26,11 @@ export const AllJokes = ({
     { field: "id", headerName: "Id" },
     { field: "type", headerName: "Type" },
     { field: "setup", headerName: "Setup", flex: 1 },
+    {
+      field: "isDisplayed",
+      headerName: "Read",
+      renderCell: (params) => <ReadIcon viewed={params.value} />,
+    },
   ];
   return (
     <Box sx={{ display: "flex", flexDirection: "column", width: "60%" }}>
