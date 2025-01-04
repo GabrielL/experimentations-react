b7b883976aacfd151d44e3e4a6ae0680287b91e9
Gabriel Laskar <gabriel@mantra.ms>
2025-01-04 13:57:26 +0100

    jokes

--- src/pages/jokes.tsx
+++ src/pages/jokes.tsx
@@ -1,4 +1,13 @@
-import { Card, CardContent, CardHeader } from "@mui/material";
+import { useMemo, useState } from "react";
+
+import {
+  Box,
+  Card,
+  CardContent,
+  Container,
+  Stack,
+  Typography,
+} from "@mui/material";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
 
 import { useAllJokesQuery } from "../api.ts";
@@ -7,34 +16,90 @@ import { Joke } from "../server/jokes.ts";
 type AllJokesProps = {
   jokes: Joke[];
   isLoading?: boolean;
+  onSelect?: (jokeId: number | null) => void;
 };
 
-const AllJokes = ({ jokes, isLoading = false }: AllJokesProps) => {
+const AllJokes = ({
+  jokes,
+  isLoading = false,
+  onSelect = () => {},
+}: AllJokesProps) => {
   const columns: GridColDef[] = [
     { field: "id", headerName: "Id" },
     { field: "type", headerName: "Type" },
-    { field: "setup", headerName: "Setup" },
+    { field: "setup", headerName: "Setup", flex: 1 },
   ];
   return (
-    <DataGrid
-      columns={columns}
-      rows={jokes}
-      loading={isLoading}
-      pageSizeOptions={[5, 10]}
-      initialState={{ pagination: { paginationModel: { pageSize: 5 } } }}
-    />
+    <Box sx={{ display: "flex", flexDirection: "column", width: "60%" }}>
+      <DataGrid
+        checkboxSelection
+        disableMultipleRowSelection
+        onRowSelectionModelChange={(row) => {
+          if (row.length == 0) {
+            onSelect(null);
+            return;
+          }
+          const id = parseInt(row[0].toString(), 10);
+          onSelect(id);
+        }}
+        columns={columns}
+        rows={jokes}
+        loading={isLoading}
+        pageSizeOptions={[5, 10]}
+        initialState={{ pagination: { paginationModel: { pageSize: 5 } } }}
+      />
+    </Box>
   );
+};
+
+const findJoke = (jokes: Joke[], jokeId: number | null) =>
+  jokes.find((joke) => joke.id == jokeId);
+
+type JokeContentProps = {
+  joke: Joke | undefined;
+};
+
+const JokeContent = ({ joke }: JokeContentProps) => {
+  if (!joke) return <>No Joke displayed</>;
+  return <>{JSON.stringify(joke)}</>;
 };
 
 export const Jokes = () => {
   const { data: jokes, isLoading } = useAllJokesQuery();
+  const [jokeId, setJokeId] = useState<number | null>(null);
+
+  const onSelect = (jokeId: number | null) => {
+    console.log("selected", jokeId);
+    setJokeId(jokeId);
+  };
+
+  const joke = useMemo(
+    () => findJoke(jokes ? jokes : [], jokeId),
+    [jokes, jokeId],
+  );
 
   return (
-    <Card>
-      <CardHeader title="jokes" />
-      <CardContent>
-        <AllJokes jokes={jokes ? jokes : []} isLoading={isLoading} />
-      </CardContent>
-    </Card>
+    <>
+      <Container>
+        <Typography variant="h2">Jokes</Typography>
+        <Stack
+          title="Jokes"
+          direction="row"
+          spacing={1}
+          alignContent="space-between"
+        >
+          <AllJokes
+            jokes={jokes ? jokes : []}
+            isLoading={isLoading}
+            onSelect={onSelect}
+          />
+          <Card sx={{ width: "50%" }}>
+            <CardContent>
+              <JokeContent joke={joke} />
+            </CardContent>
+          </Card>
+        </Stack>
+      </Container>
+    </>
   );
 };
